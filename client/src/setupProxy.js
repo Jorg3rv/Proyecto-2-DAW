ecafd1126cb4cf88c33618063d9fb8055388416a
David <22.daw.davidcarrillo@fundacionmontessori.com>
2023-05-14 20:49:57 +0200

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:5000/",
+      target: "http://18.233.219.179/",
       changeOrigin: true,
     })
   );
