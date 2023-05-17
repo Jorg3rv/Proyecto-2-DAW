73cbef6815261741c2e13d587ad77c5282404f85
Jorgerv <jruizfpb@gmail.com>
2023-05-17 22:12:21 +0200

    update itinerario

--- client/src/pages/VictoriaPage.jsx
+++ client/src/pages/VictoriaPage.jsx
@@ -1,23 +1,31 @@
-import React from "react";
-import { Link, Navigate } from "react-router-dom";
-import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
-import { colors } from "@mui/material";
-// import { ControlBar, Player } from "video-react";
-// import "video-react/dist/video-react.css";
+import React, { useContext, useEffect } from "react";
+import { ItinerarioContext } from "../context/ItinerarioContext";
+import { useNavigate } from "react-router-dom";
 
 const VictoriaPage = () => {
+  const navigate = useNavigate();
+  const { setCurrentCaso, currentCaso, itinerario } =
+    useContext(ItinerarioContext);
+
+  const handleContinuar = () => {
+    console.log("ITINERARIO: ", itinerario);
+    setCurrentCaso(currentCaso + 1);
+    if (currentCaso + 1 == itinerario.idCasos.length) {
+      navigate("/main");
+    } else {
+      navigate("/partida");
+    }
+  };
+
   return (
     <div>
       <h1 className="titulo">HAS CONSEGUIDO LAS BOTAS DE LA AMISTAD</h1>
 
-      <Link className="links" to="/equipo">
-            <button className="mi-btn-caso-redencion">
-            Continuemos
-            </button>
-          </Link>
-          </div>
+      <button onClick={handleContinuar} className="mi-btn-caso-redencion">
+        Continuemos
+      </button>
+    </div>
   );
 };
-
 
 export default VictoriaPage;
--- controllers/casoController.js
+++ controllers/casoController.js
@@ -4,7 +4,7 @@ exports.getCasoById = function (req, res) {
     let idCaso = req.params.id;
     let itinerario = req.query.itinerario;
     console.log('ITINERARIO: ', itinerario);
-    let query = `SELECT * FROM caso JOIN itinerario_caso ON caso.id_Caso = itinerario_caso.id_caso WHERE id_Itinerario = ${itinerario} AND caso.id_caso = ${idCaso}`;
+    let query = `SELECT * FROM casos JOIN itinerario_caso ON casos.id = itinerario_caso.id_caso WHERE id_itinerario = ${itinerario} AND casos.id = ${idCaso}`;
 
     connection.query(query, (error, results, fields) => {
         if (error) {
