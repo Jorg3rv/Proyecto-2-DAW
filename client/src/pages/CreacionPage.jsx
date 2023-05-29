e69f6b50b7a2d0e6bd1ad7a0d3b14c0bd7cc4f7d
David <22.daw.davidcarrillo@fundacionmontessori.com>
2023-05-29 20:29:52 +0200

    creacion
    creacion

--- client/src/pages/CreacionPage.jsx
+++ client/src/pages/CreacionPage.jsx
@@ -5,45 +5,84 @@ const CreacionPage = () => {
   const [intros, setIntro] = useState(true);
   const [mostrarBasico, setMostrarBasico] = useState(false);
   const [mostrarRedenciones, setMostrarRedenciones] = useState(false);
-  const [formData, setFormData] = useState(new FormData());
+  const id_valor='';
+  const  nombre='';
+  const  texto_intro='';
+  const  texto_Opcion_Basica='';
+  const  texto_Opcion_Avanzada='';
+  const  texto_Opcion_Pasiva='';
+  const  texto_Opcion_Agresiva='';
+  const  texto_Redencion_Pasiva='';
+  const  texto_Redencion_Buena_Pasiva='';
+  const  texto_Redencion_Mala_Pasiva='';
+  const  texto_Redencion_Agresiva='';
+  const  texto_Redencion_Buena_Agresiva='';
+  const  texto_Redencion_Mala_Agresiva='';
+  const  imagenBasica =null;
+  const  imagenAvanzada =null;
+  const  imagenAgresiva =null;
+  const  imagenPasiva =null;
+  const  imagenRedencionPasiva =null;
+  const  imagenRedencionAgresiva =null;
 
   const cambiarIntro = () => {
-    setMostrarBasico(false);
-    setIntro(true);
+    document.getElementsByClassName('opcionesBasicas')[0].style='display:none';
+    document.getElementsByClassName('opcionesRedencion')[0].style='display:none';
+    document.getElementsByClassName('opcionesIntro')[0].style='display:grid';
+    id_valor=document.getElementsByName('id_valor')[0].value;
+  nombre=document.getElementsByName('nombre')[0].value;
+  texto_intro=document.getElementsByName('texto_intro')[0].value;
   };
 
   const cambiarBasicos = () => {
-    setIntro(false);
-    setMostrarBasico(true);
-    setMostrarRedenciones(false);
+    document.getElementsByClassName('opcionesBasicas')[0].style='display:grid';
+    document.getElementsByClassName('opcionesRedencion')[0].style='display:none';
+    document.getElementsByClassName('opcionesIntro')[0].style='display:none';
+    imagenBasica =document.getElementsByName('imagenBasica')[0].files[0];
+    imagenRedencionAgresiva =document.getElementsByName('imagenRedencionAgresiva')[0].files[0];
+    imagenRedencionPasiva =document.getElementsByName('imagenRedencionPasiva')[0].files[0];
+    imagenRedencionAgresiva =document.getElementsByName('imagenRedencionAgresiva')[0].files[0];
   };
 
   const redenciones = () => {
-    setMostrarRedenciones(true);
-    setMostrarBasico(false);
-  };
-
-  const handleChange = (event) => {
-    const { name, value, files } = event.target;
-    const updatedFormData = new FormData(formData);
-    if (files) {
-      for (let i = 0; i < files.length; i++) {
-        updatedFormData.append(name, files[i]);
-      }
-    } else {
-      updatedFormData.set(name, value);
-    }
-    setFormData(updatedFormData);
+    document.getElementsByClassName('opcionesBasicas')[0].style='display:none';
+    document.getElementsByClassName('opcionesRedencion')[0].style='display:grid';
+    document.getElementsByClassName('opcionesIntro')[0].style='display:none';
+    imagenRedencionPasiva =document.getElementsByName('imagenRedencionPasiva')[0].files[0];
+    imagenRedencionAgresiva =document.getElementsByName('imagenRedencionAgresiva')[0].files[0];
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const formData = new FormData();
+    formData.append('id_valor',id_valor);
+    formData.append('nombre',nombre);
+    formData.append('texto_intro',texto_intro);
+    formData.append('texto_Opcion_Basica',texto_Opcion_Basica);
+    formData.append('texto_Opcion_Avanzada',texto_Opcion_Avanzada);
+    formData.append('texto_Opcion_Pasiva',texto_Opcion_Pasiva);
+    formData.append('texto_Opcion_Agresiva',texto_Opcion_Agresiva);
+    formData.append('texto_Redencion_Pasiva',texto_Redencion_Pasiva);
+    formData.append('texto_Redencion_Buena_Pasiva',texto_Redencion_Buena_Pasiva);
+    formData.append('texto_Redencion_Mala_Pasiva',texto_Redencion_Mala_Pasiva);
+    formData.append('texto_Redencion_Agresiva',texto_Redencion_Agresiva);
+    formData.append('texto_Redencion_Buena_Agresiva',texto_Redencion_Buena_Agresiva);
+    formData.append('texto_Redencion_Mala_Agresiva',texto_Redencion_Mala_Agresiva);
+    formData.append('imagenBasica',imagenBasica);
+    formData.append('imagenAvanzada',imagenAvanzada);
+    formData.append('imagenAgresiva',imagenAgresiva);
+    formData.append('imagenPasiva',imagenPasiva);
+    formData.append('imagenRedencionPasiva',imagenRedencionPasiva);
+    formData.append('imagenRedencionAgresiva',imagenRedencionAgresiva);
 
-    axios.post('http://54.54.54.54', formData)
-      .then(response => {
+
+
+    axios
+      .post('http://localhost:3001/pruebita', formData)
+      .then((response) => {
         console.log(response.data); // Manejar la respuesta del servidor
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error); // Manejar errores
       });
   };
@@ -51,45 +90,65 @@ const CreacionPage = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        {intros && !mostrarBasico && !mostrarRedenciones ? (
-          <div className='opcionesIntro'>
-            <label>Texto de introdución</label><textarea name="texto" placeholder="Texto Intro"  />
-            <label>Valor</label><select name="Valor" >
+
+          <div className='opcionesIntro' style={{ display: 'grid' }}>
+            <label>Texto de introducción</label>
+            <textarea name="texto_intro" placeholder="Texto Intro"   />
+            <label>Valor</label>
+            <select name="id_valor" >
               <option value="1">Amistad</option>
               <option value="2">Trabajo en equipo</option>
               <option value="3">Justicia</option>
               <option value="4">Tolerancia</option>
               <option value="5">Bondad</option>
             </select>
+            <label>Nombre</label>
+            <input type="text"  name='nombre' />
             <button onClick={cambiarBasicos}>Siguiente</button>
           </div>
-        ) : !intros && mostrarBasico && !mostrarRedenciones ? (
-          <div className='opcionesBasicas'>
-            <label>Texto Opcion Basica</label><input type="text" name="campo1"   />
-            <label>Texto Opcion Avanzada</label><input type="text" name="campo2"   />
-            <label>Texto Opcion Pasiva</label> <input type="text" name="campo3"  />
-            <label>Texto Opcion Agresiva</label><input type="text" name="campo4"   />
-            <label>Imagen Opcion Basica</label><input type="file" name="archivo1"  />
-            <label>Imagen Opcion Avanzada</label><input type="file" name="archivo2"  />
-            <label>Imagen Opcion Pasiva</label><input type="file" name="archivo1"  />
-            <label>Imagen Opcion Agresiva</label><input type="file" name="archivo2"  />
+        
+          <div className='opcionesBasicas' style={{display:'none'}}>
+            <label>Texto Opcion Basica</label>
+            <input type="text" name="texto_Opcion_Basica"   />
+            <label>Texto Opcion Avanzada</label>
+            <input type="text" name="texto_Opcion_Avanzada"   />
+            <label>Texto Opcion Pasiva</label>
+            <input type="text" name="texto_Opcion_Pasiva"   />
+            <label>Texto Opcion Agresiva</label>
+            <input type="text" name="texto_Opcion_Agresiva"   />
+            <label>Imagen Opcion Basica</label>
+            <input type="file" name="imagenBasica"   />
+            <label>Imagen Opcion Avanzada</label>
+            <input type="file" name="imagenAvanzada"   />
+            <label>Imagen Opcion Pasiva</label>
+            <input type="file" name="imagenPasiva"   />
+            <label>Imagen Opcion Agresiva</label>
+            <input type="file" name="imagenAgresiva"   />
             <button onClick={cambiarIntro}>Atrás</button>
             <button onClick={redenciones}>Siguiente</button>
           </div>
-        ) : mostrarRedenciones ? (
-          <div className='opcionesRedencion'>
-            <label>Texto Redencion Pasiva</label><textarea name="otroCampo"   />
-            <label>Texto Redencion Pasiva Buena</label><input type="text" name="masCampos"   />
-            <label>Texto Redencion Pasiva Mala</label><input type="text" name="otroCampo2"   />
-            <label>Texto Redencion Agresiva</label><textarea name="otroCampo"   />
-            <label>Texto Redencion Agresiva Buena</label><input type="text" name="masCampos"   />
-            <label>Texto Redencion Agresiva Mala</label><input type="text" name="otroCampo2"   />
-            <label>Imagen Redencion Pasiva</label><input type="file" name="otroArchivo"  />
-            <label>Imagen Redencion Agresiva</label><input type="file" name="masArchivos"  />
+
+          <div className='opcionesRedencion' style={{display:'none'}}>
+            <label>Texto Redencion Pasiva</label>
+            <textarea name="texto_Redencion_Pasiva"   />
+            <label>Texto Redencion Pasiva Buena</label>
+            <input type="text" name="texto_Redencion_Buena_Pasiva"   />
+            <label>Texto Redencion Pasiva Mala</label>
+            <input type="text" name="texto_Redencion_Mala_Pasiva"   />
+            <label>Texto Redencion Agresiva</label>
+            <textarea name="texto_Redencion_Agresiva"   />
+            <label>Texto Redencion Agresiva Buena</label>
+            <input type="text" name="texto_Redencion_Buena_Agresiva"   />
+            <label>Texto Redencion Agresiva Mala</label>
+            <input type="text" name="texto_Redencion_Mala_Agresiva"   />
+            <label>Imagen Redencion Pasiva</label>
+            <input type="file" name="imagenRedencionPasiva"   />
+            <label>Imagen Redencion Agresiva</label>
+            <input type="file" name="imagenRedencionAgresiva"  />
             <button onClick={cambiarBasicos}>Atrás</button>
             <button type='submit'>Crear</button>
           </div>
-        ) : null}
+
       </form>
     </div>
   );
